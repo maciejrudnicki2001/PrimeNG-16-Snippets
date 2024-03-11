5ef3a8d0174436d2e4788cc206fcf7ff0f2c107c
iraxus1 <76206932+iraxus1@users.noreply.github.com>
2024-03-11 15:42:29 +0100

    Completed snippets for forms

--- primeNG-Snippets/src/app/app.module.ts
+++ primeNG-Snippets/src/app/app.module.ts
@@ -2,15 +2,67 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipsModule } from 'primeng/chips';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { KnobModule } from 'primeng/knob';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { ListboxModule } from 'primeng/listbox';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { PasswordModule } from 'primeng/password';
+import { RatingModule } from 'primeng/rating';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SliderModule } from 'primeng/slider';
+import { TreeSelectModule } from 'primeng/treeselect';
+import { TriStateCheckboxModule } from 'primeng/tristatecheckbox';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AutoCompleteModule,
+    CalendarModule,
+    CascadeSelectModule,
+    CheckboxModule,
+    ChipsModule,
+    ColorPickerModule,
+    DropdownModule,
+    InputTextModule,
+    RadioButtonModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextareaModule,
+    InputNumberModule,
+    KnobModule,
+    KeyFilterModule,
+    ListboxModule,
+    MultiSelectModule,
+    PasswordModule,
+    RatingModule,
+    SelectButtonModule,
+    SliderModule,
+    TreeSelectModule,
+    TriStateCheckboxModule,
+    ToggleButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
